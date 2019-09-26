3e835764cf413c211161dd04c7f521db7e7552af
hassehulabeck <hansbudandersson@gmail.com>
2019-09-26 13:12:41 +0200

    Lotto-2

--- lotto.js
+++ lotto.js
@@ -1,10 +1,19 @@
 function getLottoRad() {
     var numbers = [];
     for (i = 0; i < 7; i++) {
-        let slump = Math.floor(Math.random() * 35);
-        numbers.push(slump);
+        let slump = Math.ceil(Math.random() * 35);
+        if (numbers.includes(slump)) {
+            i--; // Manuellt minska iteratorn i för att en iteration har "gått förlorad", då vi slumpade fram ett tal som redan fanns.
+        } else {
+            numbers.push(slump);
+        }
     }
+    // Sortera arrayen för bättre läsbarhet.
+    numbers.sort(function (a, b) {
+        return a - b;
+    });
     return numbers;
 }
 
 var lottorad = getLottoRad();
+console.log(lottorad);
